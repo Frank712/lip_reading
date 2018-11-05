4639ba827f2a8284eb6a0ecd91c7a7ae3339ebc9
Frank712 <franksuriel_7@hotmail.com>
2018-11-05 17:01:02 -0600

    Change in the gen_path_dir.sh

--- gen_path_word.sh
+++ gen_path_word.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 dir='dataWords'
-ls ./save > tmp1.txt
+#ls ./save > tmp1.txt
 
-sed -e 's/_.*//g' tmp1.txt > tmp2.txt
+#sed -e 's/_.*//g' tmp1.txt > tmp2.txt
 
-cat tmp2.txt | sort | uniq > w_paths.txt
+#cat tmp2.txt | sort | uniq > w_paths.txt
 
-rm tmp1.txt
-rm tmp2.txt
+#rm tmp1.txt
+#rm tmp2.txt
 
 if [ ! -d ${dir} ]; then
 mkdir ${dir}
